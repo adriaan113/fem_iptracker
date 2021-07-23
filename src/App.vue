a3cc9f25ffa4225a8a714ce27ab05084423ad0b0
Adriaan van der Ploeg <adriaan@MacBook-Air-van-Adriaan.local>
2021-07-23 20:51:11 +0200

    added all mobile css

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <template>
   <div id="app">
-    <h1 class="title">IP Address Tracker</h1>
-    <img class="background-img" :src="backgroundImg" alt="">
-    <HelloWorld :image="map"/>
+    <header>
+      <h1 class="title">IP Address Tracker</h1>
+    </header>
+    
+    <!-- <img class="background-img" :src="backgroundImg" alt=""> -->
+    <HelloWorld/>
   </div>
 </template>
 
@@ -16,23 +19,33 @@ export default {
   },
   data: function(){
     return{
-      backgroundImg: require('/src/assets/pattern-bg.png'),
-      map: require('/src/assets/Maps.png')
+      // backgroundImg: require('/src/assets/pattern-bg.png'),
+      // map: require('/src/assets/Maps.png')
     }
   },
 }
 </script>
 
 <style lang="scss">
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap');
 @import "./global.scss";
 
 html,
 body{
   margin: 0;
   padding: 0;
-
 }
+
+h1{
+  font-size: $h1-mobile-font-size;
+}
+h2{
+  font-size: $h2-mobile-font-size;
+}
+p{
+  font-size: $p-mobile-font-size;
+}
+
 #app {
   font-family: 'Roboto', sans-serif;
   font-size: 16px;
@@ -41,15 +54,21 @@ body{
   display: flex;
   flex-flow: column nowrap;
   align-items: center;
-  .title{
-    margin: 0 auto;
-    position: absolute;
-    color: $primary-text-color;
-  }
-  .background-img{
+  header{
     width: 100%;
     height: 33vh;
-    //height: 100%;
+    background-image: url("./assets/pattern-bg.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: flex;
+    flex-flow: column nowrap;
+    align-items: center;
+    .title{
+      font-size: $h1-mobile-font-size;
+      position: absolute;
+      color: $primary-text-color;
+      font-weight: lighter;
+    }
   }
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -25,7 +25,7 @@
       </li>
     </ul>
     <div class="map">
-      <img :src="image" alt="">
+      <!-- <img :src="image" alt=""> -->
     </div>
   </div>
 </template>
@@ -34,7 +34,7 @@
 export default {
   name: 'HelloWorld',
     props:{
-      image: Object,
+      // image: Object,
   }
  
 }
@@ -52,31 +52,70 @@ export default {
       position: absolute;
       top: 10%;
       width: 100%;
+      max-width: 35rem;
       display: flex;
       justify-content: center;
       input{
-        height: 2rem;
+        height: 2.5rem;
         margin: 0;
         padding: 0;
         border-width: 0;
       }
       input[type="text"]{
-        width: 80%;
+        width: 80vw;
         border-radius: $border 0 0 $border;
       }
       input[type="submit"]{
         width: 10%;
+        max-width: 3rem;
         background-color: $btn-color;
         color: $primary-text-color;
         border-radius:  0 $border $border 0;
+        cursor: pointer;
+        &:hover{
+          background-color: $btn-hover-color;
+        }
+      }
+    }
+
+    .info{
+      background-color: $background-color;
+      list-style: none;
+      border-radius: $border;
+      display: flex;
+      flex-flow: column nowrap;
+      align-items: center;
+      padding: 1rem 0;
+      margin:  -5rem 0 0;
+      width: 90vw;
+      max-width: 69rem;
+      z-index: 2;
+      &__item{
+        display: flex;
+        flex-flow: column nowrap;
+        align-items: center;
+        width: 100%;
+        p{
+          color: $tertiary-text-color;
+        }
+        h2{
+          margin: 0 0 .5rem;
+        }
+        &:nth-child(1){
+          h2{
+            font-weight: lighter;
+          }
+        }
       }
     }
 
     .map{
-      img{
-      width: 100%;
-      height: auto;
-      }  
+      background-image: url("../assets/Maps.png");
+        background-repeat: no-repeat;
+        background-size: cover;
+        width: 100%;
+        height: 67vh;
+        position: absolute; 
   }
 
 }
